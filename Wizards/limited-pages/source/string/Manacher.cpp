2b30babf53b2570013d3735a8e89e6709f75d71d
lgkm39 <209985587@qq.com>
2017-11-20 18:05:23 +0800

    fix some bugs

--- Wizards/limited-pages/source/string/Manacher.cpp
+++ Wizards/limited-pages/source/string/Manacher.cpp
@@ -1,10 +1,9 @@
 void Manacher(std::string s,int p[]) {
-    string t = "$#";
+    std::string t = "$#";
     for (int i = 0; i < s.size(); i++) {
         t += s[i];
         t += "#";
     }
-    std::vector<int> p(t.size(), 0);
     int mx = 0, id = 0;
     for (int i = 1; i < t.size(); i++) {
         p[i] = mx > i ? min(p[2 * id - i], mx - i) : 1;
