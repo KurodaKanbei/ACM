132279b64b62203bc8146e1cf96a3ad6fb11bb2f
lgkm39 <209985587@qq.com>
2017-11-20 18:28:29 +0800

    recover from older commitment

--- Wizards/limited-pages/source/graph-theory/2SAT.cpp
+++ Wizards/limited-pages/source/graph-theory/2SAT.cpp
@@ -1,46 +1,26 @@
-int rev(int x)
-{
-	if(x & 1) return x + 1;
-	return x - 1;
+int rev(int x) {
+	return (x & 1) ? x + 1 : x - 1;
 }
-bool dfs(int x)
-{
+bool dfs(int x) {
 	if(used[x]) return true;
 	if(used[rev(x)]) return false;
-	
-	used[x] = true;
-	vis.push_back(x);
-	
+	used[x] = true, vis.push_back(x);
 	for(int i = 0; i < vec[x].size(); i++)
 		if(!dfs(vec[x][i])) return false;
-	
 	return true;
 }
-bool check(int x)
-{
-	vis.clear();
-	
+bool check(int x) {
 	bool res = dfs(x);
-	
-	for(int i = 0; i < vis.size(); i++)
-		used[vis[i]] = false;
-	
+	vis.clear();
+	for(int i = 0; i < vis.size(); i++) used[vis[i]] = false;
 	return res;
 }
-bool work()
-{
+bool work() {
 	bool ans = true;
-	
-	for(int i = 1; i <= n; i++)
-	{
+	for(int i = 1; i <= n; i++) {
 		if(check(2 * i - 1)) dfs(2 * i - 1);
 		else if(check(2 * i))  dfs(2 * i);
-		else
-		{
-			ans = false;
-			break;
-		}
+		else { ans = false; break; }
 	}
-		
 	return ans;
 }
--- Wizards/limited-pages/source/graph-theory/dinic.cpp
+++ Wizards/limited-pages/source/graph-theory/dinic.cpp
@@ -1,18 +1,19 @@
 bool BFS() {
     for(int i = 1; i <= ind; i++) dep[i] = 0;
+    
     dep[S] = 1, line.push(S);
     while(!line.empty()) {
-        int now = line.front();
-        line.pop();
+        int now = line.front(); line.pop();
         for(int i = head[now], p; i ; i = edge[i].next)
             if(edge[i].cap && !dep[p = edge[i].v])
                 dep[p] = dep[now] + 1, line.push(p);
     }
+
     if(dep[T]) {
-        for(int i = 1; i <= ind; i++)
-            cur[i] = head[i];
+        for(int i = 1; i <= ind; i++) cur[i] = head[i];
         return true;    
-    } else return false;
+    }
+    else return false;
 }
 int DFS(int a,int flow) {
     if(a == T) return flow;
@@ -24,11 +25,11 @@ int DFS(int a,int flow) {
             ret += ff, edge[i ^ 1].cap += ff;
             if(!flow) break;
         }
-        return ret;
+	return ret;
 }
 int solve() {
     int totflow = 0;
-    while(BFS())
+    while(BFS()) {
         totflow += DFS(S, INF);
     }
     return totflow;
