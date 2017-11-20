ebe31ac9557bfc2776f207d2faae0284b2cc0e1d
范裕达 <812395846@qq.com>
2017-11-20 17:59:26 +0800

    more and more

--- Wizards/limited-pages/source/number-theory/Pell.java
+++ Wizards/limited-pages/source/number-theory/Pell.java
@@ -1,13 +1,5 @@
 import java.math.BigInteger;  
 import java.util.Scanner;  
-//a[n]=(g[n]+a[0])/h[n]  
-//g[n]=a[n-1]*h[n-1]-g[n-1]  
-//h[n]=(N-g[n]*g[n])/h[n-1]  
-//p[n]=a[n-1]*p[n-1]+p[n-2]  
-//q[n]=a[n-1]*q[n-1]+q[n-2]  
-//so:  
-//p[n]*q[n-1]-p[n-1]*q[n]=(-1)^(n+1);  
-//p[n]^2-N*q[n]^2=(-1)^(n+1)*h[n+1];  
 public class Main {  
     public static BigInteger p, q;  
     public static void solve(int n) {  
@@ -22,13 +14,13 @@ public class Main {
             a2 = g2.add(a0).divide(h2);
             p = a1.multiply(p2).add(p1); 
             q = a1.multiply(q2).add(q1);
-            if (p.pow(2).subtract(N.multiply(q.pow(2))).compareTo(BigInteger.ONE) == 0) return;
+            if (p.pow(2).subtract(N.multiply(q.pow(2)))
+				.compareTo(BigInteger.ONE) == 0) return;
 			g1 = g2;h1 = h2;a1 = a2;  
             p1 = p2;p2 = p;  
             q1 = q2;q2 = q;  
         }  
     }  
-   
     public static void main(String[] args) {  
         Scanner cin = new Scanner(System.in);  
         int t=cin.nextInt();  
--- Wizards/limited-pages/source/others/rader.cpp
+++ Wizards/limited-pages/source/others/rader.cpp
@@ -1,35 +1,29 @@
-int h()
-{
+int h() {
     int i,j,k,count=0;
     bool visit[N];
     memset(visit,0,sizeof(visit));
-    for(i=R[0];i;i=R[i])
-    {
+    for(i=R[0];i;i=R[i]) {
         if(visit[i]) continue;
         count++;
         visit[i]=1;
-        for(j=D[i];j!=i;j=D[j])
-        {
+        for(j=D[i];j!=i;j=D[j]) {
             for(k=R[j];k!=j;k=R[k])
                 visit[C[k]]=1;
         }
     }
     return count;
 }
-void Dance(int k)
-{
+void Dance(int k) {
     int i,j,c,Min,ans;
     ans=h();
     if(k+ans>K || k+ans>=ak) return;
-    if(!R[0])
-    {
+    if(!R[0]) {
         if(k<ak) ak=k;
         return;
     }
     for(Min=N,i=R[0];i;i=R[i])
         if(S[i]<Min) Min=S[i],c=i;
-    for(i=D[c];i!=c;i=D[i])
-    {
+    for(i=D[c];i!=c;i=D[i]) {
         remove(i);
         for(j=R[i];j!=i;j=R[j])
             remove(j);
